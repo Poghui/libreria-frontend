9e53ed0b954c3d42ebfd93953a7f8b2874ee3a41
Marc Portero <poghui4@gmail.com>
2024-06-13 18:07:26 +0200

    subida-front

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import TableComponent from './TableComponent';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Tabla de Productos</h1>
+                <TableComponent />
+            </header>
+        </div>
+    );
+}
+
+export default App;
