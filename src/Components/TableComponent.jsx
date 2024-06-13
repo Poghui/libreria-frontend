8b25c0cfe35873e4d9467814c41707af3a81c557
FerNumbers <seriouschocodrilo@gmail.com>
2024-06-13 19:54:50 +0200

    ferrrgulicious

--- src/Components/TableComponent.jsx
+++ src/Components/TableComponent.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { createLibro } from "../app/services/libros";
 
 
 const TableComponent = () => {
@@ -7,8 +9,14 @@ const TableComponent = () => {
         { id: 2, titulo: 'Producto 2', precio: 150 },
         { id: 3, titulo: 'Producto 3', precio: 200 },
     ];
+    const [id, setId] = useState()
+    const [nombre, setNombre] = useState()
+    const [precio, setPrecio] = useState()
 
     return (
+        <>
+        {/* Al ser el parametro a enviar igual nombre de estado/variable/constante que enviamos podemos abreviarlo: (nombre : nombre, precio : precio ) pasa a ser (nombre, precio) */}
+        <button onClick={()=> createLibro({nombre, precio})}>Create Libro</button>
         <table>
             <thead>
                 <tr>
@@ -31,6 +39,14 @@ const TableComponent = () => {
                 ))}
             </tbody>
         </table>
+        <table>
+            <tr>
+                <td><input type="number" placeholder="Id" onChange={e => setId(e.target.value)}/></td>
+                <td><input type="text" placeholder="Titulo" onChange={e => setNombre(e.target.value)}/></td>
+                <td><input type="number" placeholder="Precio" onChange={e => setPrecio(e.target.value)}/></td>
+            </tr>
+        </table>
+        </>
     );
 };
 
